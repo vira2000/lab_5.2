0ab9573a868f39c80bd359cf5cc361d9140258d3
vira2000 <73272437+vira2000@users.noreply.github.com>
2020-12-03 06:41:12 +0200

    first master commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,55 @@
+// Лабораторна робота No 5.2
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "exp(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << exp(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+
+			<< endl;
+
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+void S(const double x, const double eps, int& n, double& s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = -1; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps); 
+}
+void A(const double x, const int n, double& a)
+{
+	double R = 1/((2*n+1)*pow(x,2*n+1)); // вираз залежить від умови завдання варіанту
+	a *= R;
+}
